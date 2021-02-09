3e352d198126e5f449c6423b6ff8bd066643a5e1
Poon Nguyen <nguyenduypoon@gmail.com>
2021-02-09 10:55:45 +0700

    [Feature] Add request logger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import chalk from 'chalk';
+import { Request, Response } from 'express';
+import morgan from 'morgan';
 
 import AppModule from './AppModule';
 import ApplicationConfig from './infrastructure/configs/ApplicationConfig';
+import { isInternalUrl } from './infrastructure/modules/render/next-utils';
 
 const enableCors = (app: NestExpressApplication, nodeEnv: string, port: number) => {
   if (nodeEnv === 'production') {
@@ -17,6 +21,26 @@ const enableCors = (app: NestExpressApplication, nodeEnv: string, port: number) 
   }
 };
 
+const bindRequestLogger = (app: NestExpressApplication) => {
+  const logFormat: morgan.FormatFn<Request, Response> = (tokens, req, res) =>
+    [
+      chalk.yellowBright(`[Request] - ${process.pid.toString()}`),
+      tokens.date(req, res),
+      chalk.magenta(`${tokens.method(req, res)}`),
+      chalk.white(tokens.url(req, res)),
+      chalk.white(tokens.status(req, res)),
+      chalk.white(tokens.res(req, res, 'content-length')),
+      `${chalk.green(`+${tokens['response-time'](req, res)}ms`)}`
+    ].join(' ');
+  app.use(
+    morgan(logFormat, {
+      skip(req) {
+        return isInternalUrl(req.url);
+      }
+    })
+  );
+};
+
 (async () => {
   try {
     const applicationConfig = ApplicationConfig.getInstance();
@@ -25,6 +49,7 @@ const enableCors = (app: NestExpressApplication, nodeEnv: string, port: number) 
     const port = parseInt(configService.get<string>(applicationConfig.getPortProp()) ?? '3000');
     const nodeEnv = configService.get<string>(applicationConfig.getNodeEnvProp()) ?? '';
     enableCors(app, nodeEnv, port);
+    bindRequestLogger(app);
     await app.listen(port);
   } catch (error) {
     throw new InternalServerErrorException(error);
