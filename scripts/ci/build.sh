44f643e924d909c6fd8ee57eedf377c77ea36a64
Poon Nguyen <nguyenduypoon@gmail.com>
2021-04-02 18:49:56 +0700

    [Update] Remove prisma generate deploy in build script

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -5,10 +5,6 @@ printf "=> Generate prisma types"
 yarn --cwd api prisma generate --schema ./prisma/schema.prisma
 yarn --cwd api prisma generate --schema ./prisma/schema.test.prisma
 
-printf "=> Deploy prisma database"
-yarn --cwd api prisma migrate deploy --schema ./prisma/schema.prisma
-yarn --cwd api prisma migrate deploy --schema ./prisma/schema.test.prisma
-
 echo "=> Build API application"
 yarn rimraf "api/dist"
 yarn --cwd api nest build
