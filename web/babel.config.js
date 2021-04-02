2a4ddd082f46f16ae0010e0421664ac5fbaf1725
Poon Nguyen <nguyenduypoon@gmail.com>
2021-04-02 18:15:24 +0700

    [Fix] Refactor babel.config.js

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,15 +1,11 @@
-{
-  "presets": [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript"
-  ],
-  "plugins": [
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+  plugins: [
     [
-      "@babel/plugin-transform-runtime",
+      '@babel/plugin-transform-runtime',
       {
-        "regenerator": true
+        regenerator: true
       }
     ]
   ]
-}
+};
