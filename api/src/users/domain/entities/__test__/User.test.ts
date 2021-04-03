58f9badad27dc8dff5befeaf4d1d08ab0a3afe25
Poon Nguyen <nguyenduypoon@gmail.com>
2021-04-03 12:11:32 +0700

    [New] Implement unit test for User.test.ts

---
+++ api/src/users/domain/entities/__test__/User.test.ts
@@ -0,0 +1,125 @@
+import UserType from '../../graphql/UserType';
+import Age from '../../models/Age';
+import Id from '../../models/Id';
+import Name from '../../models/Name';
+import User from '../User';
+
+describe('User', () => {
+  const user: User = new User({
+    id: 1,
+    name: 'foo',
+    age: 2,
+    createdAt: new Date(2021, 4, 3),
+    updatedAt: new Date(2021, 4, 4)
+  });
+
+  describe('get id', () => {
+    it('should get correct id value', () => {
+      const expectedId = new Id(1);
+
+      const id = user.id;
+
+      expect(id).toMatchObject(expectedId);
+    });
+  });
+
+  describe('set id', () => {
+    it('should set correct id value', () => {
+      const setId = new Id(2);
+
+      user.id = setId;
+
+      expect(user.id).toMatchObject(setId);
+    });
+  });
+
+  describe('get name', () => {
+    it('should get correct name value', () => {
+      const expectedName = new Name(user.name.value);
+
+      const name = user.name;
+
+      expect(name).toMatchObject(expectedName);
+    });
+  });
+
+  describe('set name', () => {
+    it('should set correct name value', () => {
+      const name = new Name('baz');
+
+      user.name = name;
+
+      expect(user.name).toMatchObject(name);
+    });
+  });
+
+  describe('get age', () => {
+    it('should get correct age value', () => {
+      const expectedAge = new Age(user.age.value);
+
+      const age = user.age;
+
+      expect(age).toMatchObject(expectedAge);
+    });
+  });
+
+  describe('set age', () => {
+    it('should set correct age value', () => {
+      const age = new Age(3);
+
+      user.age = age;
+
+      expect(user.age).toMatchObject(age);
+    });
+  });
+
+  describe('get createdAt', () => {
+    it('should get correct createdAt value', () => {
+      const createdAt = user.createdAt;
+
+      expect(createdAt).toMatchObject(new Date(2021, 4, 3));
+    });
+  });
+
+  describe('set createdAt', () => {
+    it('should set correct createdAt value', () => {
+      const createdAt = new Date();
+
+      user.createdAt = createdAt;
+
+      expect(user.createdAt).toMatchObject(createdAt);
+    });
+  });
+
+  describe('get updatedAt', () => {
+    it('should get correct updatedAt value', () => {
+      const updatedAt = user.updatedAt;
+
+      expect(updatedAt).toMatchObject(new Date(2021, 4, 4));
+    });
+  });
+
+  describe('set updatedAt', () => {
+    it('should set correct updatedAt value', () => {
+      const updatedAt = new Date();
+
+      user.updatedAt = updatedAt;
+
+      expect(user.updatedAt).toMatchObject(updatedAt);
+    });
+  });
+
+  describe('convertToGraphql', () => {
+    const expected: UserType = {
+      id: 1,
+      name: 'foo',
+      age: 2,
+      createdAt: new Date(2021, 4, 3),
+      updatedAt: new Date(2021, 4, 4)
+    };
+
+    const result = user.convertToGraphql();
+
+    expect(result).toMatchObject(expected);
+  });
+});
