2dab08844ef59d4e0d311c4a810c60bbe42bc6c9
Poon Nguyen <nguyenduypoon@gmail.com>
2021-04-03 11:52:17 +0700

    [New] Implement unit test for UserResolver.test.ts

--- api/test_helpers/TestHelpersModule.ts
+++ api/test_helpers/TestHelpersModule.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 
+import CommonModule from '../src/common/CommonModule';
 import PrismaService from './services/PrismaService';
 
 @Global()
 @Module({
+  imports: [CommonModule],
   providers: [PrismaService],
   exports: [PrismaService]
 })
